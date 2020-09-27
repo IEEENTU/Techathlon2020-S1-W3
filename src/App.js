292e42bbdcfbd6d35bdf9f2e28c6a2f37b3945be
Conalier <akshitkjain@gmail.com>
2020-09-28 01:25:59 +0800

    Finished Mock Design

--- src/App.js
+++ src/App.js
@@ -3,27 +3,31 @@ import Header from "./components/header.js";
 import Platform from "./components/platform.js";
 import "./App.css";
 import sample from "./images/Gintoki.png";
+import FbPic from "./images/facebook.jpg";
+import IgPic from "./images/instagram.png";
+import LiPic from "./images/linkedin.png";
+import GmPic from "./images/gmail.jpg";
 
 function App() {
   const socialMedia = [
     {
       name: "Facebook",
-      image: "./images/facebook.jpg",
+      image: FbPic,
       url: "#",
     },
     {
       name: "Instagram",
-      image: "./images/instagram.png",
+      image: IgPic,
       url: "#",
     },
     {
       name: "LinkedIn",
-      image: "./images/linkedin.png",
+      image: LiPic,
       url: "#",
     },
     {
       name: "Gmail",
-      image: "./images/gmail.jpg",
+      image: GmPic,
       url: "#",
     },
   ];
@@ -43,13 +47,25 @@ function App() {
           </div>
 
           <div id="gallery" className="item">
-            <img src={sample} alt="" />
+            <img id="pic1" src={sample} alt="" />
+            <img id="pic2" src={sample} alt="" />
+            <img id="pic3" src={sample} alt="" />
+            <img id="pic4" src={sample} alt="" />
+            <img id="pic5" src={sample} alt="" />
+            <img id="pic6" src={sample} alt="" />
+            <img id="pic7" src={sample} alt="" />
           </div>
 
           <div id="caption" className="item">
             <div id="symbol">"</div>
-            <div id="caption-content">This is the caption</div>
+            <div id="caption-content">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae
+              architecto ea blanditiis sunt sint atque magnam, qui,
+              necessitatibus totam, saepe modi. Cum commodi reprehenderit
+              molestiae deleniti aut odio voluptatem error?
+            </div>
           </div>
+          <br />
 
           <div id="social-media" className="item">
             {socialMedia.map((social) => (
@@ -59,6 +75,11 @@ function App() {
                 url={social.url}
               />
             ))}
+          </div>
+
+          <div id="tagline" className="item">
+            Check out our social media pages for more cool projects by IEEE NTU
+            Student Branch!
           </div>
         </div>
       </div>
